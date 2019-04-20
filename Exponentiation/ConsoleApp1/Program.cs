71278de0d63dc06a21ff214529fb80e7aa7bd60f
chunchi-huang <ryancc.huang0729@gmail.com>
2019-04-20 11:28:44 -0700

    print exponentiation result

---
+++ Exponentiation/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Recursive Algorithms - Exponentiation");
+
+            int answer = 1;
+
+            Console.WriteLine("Please input the base number:");
+            int.TryParse(Console.ReadLine(), out int baseNumber);
+
+            Console.WriteLine("Please input the exp number:");
+            int.TryParse(Console.ReadLine(), out int exp);
+
+            for (int i = 1; i <= exp; i++)
+                answer = answer * baseNumber;
+            
+            Console.WriteLine("Base = {0}, Exp = {1}", baseNumber, exp);
+            Console.WriteLine("Answer = {0}", answer);
+            Console.ReadLine();
+        }
+    }
+}
